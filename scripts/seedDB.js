dd677000b09cb639451c4103f8523c94c32a278b
MelanieBott2691 <melaniebott@gmail.com>
2020-07-16 13:49:38 -0600

    set up empty seeds to test

---
+++ scripts/seedDB.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const db = require('../models')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlesearch')
+const bookSeed = {
+  authors: [],
+  description: '',
+  image: '',
+  link: '',
+  title: ''
+}
+
+db.Book.remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + ' records inserted!')
+    process.exit(0)
+  })
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
