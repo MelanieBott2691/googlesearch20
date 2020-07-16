6776e39f94ff4d3e8ce7e21c6f2113417f1efb94
MelanieBott2691 <melaniebott@gmail.com>
2020-07-16 14:52:08 -0600

    added to build for both public and build in json and changed const appDirectory in node_modules but now react script start is having err

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const apiRoutes = require('./api')
 // API Routes
 router.use('/api', apiRoutes)
 router.use(function (req, res) {
-  res.sendFile(path.join(__dirname, './client/index.html'))
+  res.sendFile(path.join(__dirname, 'client/public/index.html'))
 })
 module.exports = router
