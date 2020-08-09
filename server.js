cc5a2dfe8af086dce9c8acb21c8c3aa5c1c6ffba
MelanieBott2691 <melaniebott@gmail.com>
2020-08-08 21:22:31 -0600

    commented out app.get function on lines 21-23 and app is working fine again, app still working will try to push to heroku again and see if the connection is fixed

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
+const express = require('express');
+const mongoose = require('mongoose');
+// const path = require('path')
 
-const PORT = process.env.PORT || 3000
-const app = express()
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-const routes = require('./routes')
+const routes = require('./routes');
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static('client/build'));
 }
 
 // Define API routes
-app.use(routes)
+app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, './client/build/index.html'))
+// })
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlesearch')
-// { useNewUrlParser: true }
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlesearch',
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}!`)
-})
+  console.log(`App listening on PORT ${PORT}!`);
+});
