369b9563dc9f9eff749b27dbaa71f20c336d1277
MelanieBott2691 <melaniebott@gmail.com>
2020-08-20 23:24:51 -0600

    Updates

---
+++ precache-manifest.c5e125281297baa8705ebf02ad84a444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe987ade2eebde42d27ce06df1eebbe5",
+    "url": "/googlesearch20/index.html"
+  },
+  {
+    "revision": "e9c41c7522ba80e695b7",
+    "url": "/googlesearch20/static/css/main.f14841ff.chunk.css"
+  },
+  {
+    "revision": "4605513ed0faf1bba771",
+    "url": "/googlesearch20/static/js/2.528789de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/googlesearch20/static/js/2.528789de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c41c7522ba80e695b7",
+    "url": "/googlesearch20/static/js/main.69489955.chunk.js"
+  },
+  {
+    "revision": "acec8f930550e2850d21",
+    "url": "/googlesearch20/static/js/runtime-main.691aa077.js"
+  },
+  {
+    "revision": "4c71462e9f58a8668f9667aab4f8543e",
+    "url": "/googlesearch20/static/media/header.4c71462e.jpg"
+  }
+]);
